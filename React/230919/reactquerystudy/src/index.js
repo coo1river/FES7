92850ad39ddadf9f5b1e0d63230708fea3edee6f
coolriver <longpaca@gmail.com>
2023-09-19 23:13:26 +0900

    react query 연습

---
+++ React/230919/reactquerystudy/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
