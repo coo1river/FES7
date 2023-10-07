857d05a258fdd860a4015858b52e4710c6d4fcb2
coo1river <longpaca@gmail.com>
2023-10-08 04:47:39 +0900

    JoinPage 초기 세팅

--- React/231006/app/src/App.js
+++ React/231006/app/src/App.js
@@ -1,10 +1,12 @@
+import JoinPage from "./Components/JoinPage";
 import LoginPage from "./Components/LoginPage";
 
 function App() {
   // 로그인 기능!
   return (
     <div>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <JoinPage />
     </div>
   );
 }
