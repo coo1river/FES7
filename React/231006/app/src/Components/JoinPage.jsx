674381dbeb21a371d57dc7755374297d44d2bf3d
coo1river <longpaca@gmail.com>
2023-10-08 05:40:18 +0900

    중간 커밋

--- React/231006/app/src/Components/JoinPage.jsx
+++ React/231006/app/src/Components/JoinPage.jsx
@@ -5,10 +5,22 @@ const JoinPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [accountname, setAccountname] = useState("");
+  const [imgSrc, setImgSrc] = useState(
+    "https://api.mandarin.weniv.co.kr/Ellipse.png"
+  );
 
   // 회원가입 함수 만들기
-  const join = (joinData) => {
-    console.log(joinData);
+  const join = async (joinData) => {
+    const reqUrl = "https://api.mandarin.weniv.co.kr/user";
+    const res = await fetch(reqUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(joinData),
+    });
+    const json = await res.json();
+    console.log(json);
   };
 
   const inputUsername = (e) => {
@@ -27,12 +39,34 @@ const JoinPage = () => {
     setAccountname(e.target.value);
   };
 
+  const uploadImage = async (imageFile) => {
+    const reqUrl = "https://api.mandarin.weniv.co.kr/image/uploadfile";
+    // 폼 데이터 만들기
+    const form = new FormData();
+    // 폼 데이터에 값 추가하기
+    // 폼 데이터.append("키", "값")
+    form.append("image", imageFile);
+    // 폼 바디에 넣어서 요청하기
+    const res = await fetch(reqUrl, {
+      method: "POST",
+    });
+  };
+
+  const handleChangeImage = (e) => {
+    // 파일 가져오기
+    const imageFile = e.target.files[0];
+    uploadImage(imageFile);
+  };
+
   const submitJoin = (e) => {
     e.preventDefault();
     const joinData = {
-      username: username,
-      password: password,
-      accountname: accountname,
+      user: {
+        username: username,
+        email: email,
+        password: password,
+        accountname: accountname,
+      },
     };
     join(joinData);
   };
@@ -70,11 +104,7 @@ const JoinPage = () => {
         <h2>프로필 설정</h2>
         <p>나중에 언제든지 변경할 수 있습니다.</p>
         <label htmlFor="profileImg">
-          <img
-            src="https://api.mandarin.weniv.co.kr/Ellipse.png"
-            alt=""
-            id="imagePre"
-          />
+          <img src={imgSrc} alt="" id="imagePre" />
         </label>
         <input type="file" id="profileImg" name="image" accept="image/*" />
         <div>
